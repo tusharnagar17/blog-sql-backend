e092e4db4f658a253303620cde85f031bb4bae57
Tushar Nagar <nagartushar771@gmail.com>
2024-01-24 20:01:21 +0530

    accessing data without error

--- ping.js
+++ ping.js
@@ -1,5 +1,6 @@
+
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   // use this dialer option for only heroku
@@ -15,13 +16,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 const main = async () => {
   try {
     await sequelize.authenticate();
-    const data = await sequelize.query("SELECT * FROM blogs", {
-      type: QueryTypes.SELECT,
-    });
-    // console.log(data);
-    data.map((item) => {
-      console.log(item.author, item.title, item.likes, " likes");
-    });
     console.log("Connection has been established successfully.");
     sequelize.close();
   } catch (error) {
